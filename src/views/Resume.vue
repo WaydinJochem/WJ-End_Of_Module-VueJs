3b3f79e5d35b0a06294c21137b6e054614a1aac8
WaydinJochem <jochem7405@gmail.com>
2023-07-06 14:02:43 +0200

    changes

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,5 +1,12 @@
 <template>
-    <NavAll/>
+    <NavAll />
+    <div>
+        <div v-if="projects">
+            <div v-for="projects in projects" :key="projects.id" :projects="projects">
+            </div>
+        </div>
+        <div v-else class="text-center">Waiting To show</div>
+    </div>
 </template>
 <script>
 import NavAll from '@/components/NavBar/NavBarAll.vue';
