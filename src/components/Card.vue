562e351f5c1ca23ca698092e6715133917a5daf1
WaydinJochem <jochem7405@gmail.com>
2023-07-06 12:24:20 +0200

    Changes

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,22 @@
 <template>
     <div v-if="project">
         <div v-for="projects in projects" :key="projects.id" :projects="projects">
+            <!-- Card Start -->
+            <div class="container-fluid p-3">
+                <h2 class="playball text-center">Projects</h2>
+                <!-- Project Card 1 -->
+                <div class="flip-card col-md-4 glow">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <img src="./Assets/icons/icons8-1-64.png" alt="" class="cardpic">
+                        </div>
+                        <div class="flip-card-back align-middle">
+                            <a class="btn btn-info" href="http://waydinbootstrap.netlify.app">Launch Site</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Card End -->
         </div>
     </div>
     <div v-else class="text-center">Waiting To show</div>
