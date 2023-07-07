acca8fbad8f970c04d64535b9f2dd22ce4064aaa
WaydinJochem <jochem7405@gmail.com>
2023-07-07 23:09:04 +0200

    Changes

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -7,7 +7,13 @@
         </div>
         <div v-else class="text-center">Waiting To show</div>
     </div>
+    <button>
+        <a class="display-6" href="https://drive.google.com/uc?export=download&id=your_CV_ID_FROM_Google_Drive"
+            target="_blank">
+        </a>
+    </button>
 </template>
+    Download CV
 <script>
 import NavAll from '@/components/NavBar/NavBarAll.vue';
 export default {
